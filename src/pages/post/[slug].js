b2e4584229e4d9a37120b9d7f1602b890759386a
wepypixel <wepypixel@gmail.com>
2023-06-03 22:49:26 +0530

    Added Social Shares

--- src/pages/post/[slug].js
+++ src/pages/post/[slug].js
@@ -1,8 +1,14 @@
 import axios from "axios";
 import { format } from "date-fns";
 import { useEffect } from "react";
-import { AiFillClockCircle, AiFillTwitterCircle, AiOutlineWhatsApp } from "react-icons/ai";
-import { BsTelegram, BsWhatsapp } from "react-icons/bs";
+import { AiFillClockCircle } from "react-icons/ai";
+import {
+  BsTelegram,
+  BsWhatsapp,
+  BsTwitter,
+  BsFacebook,
+  BsLinkedin,
+} from "react-icons/bs";
 import DOMPurify from "isomorphic-dompurify";
 import Link from "next/link";
 import Head from "next/head";
@@ -30,6 +36,35 @@ export default function Post({ blogPost, relatedPosts, popularPosts }) {
     return truncated.slice(0, lastSpace) + "...";
   }
 
+  function shareOnFacebook() {
+    // Replace "YOUR_URL" with the actual URL of your blog post
+    var url = `https://pypixel.com/post/${blogPost.slug}`;
+    window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url));
+  }
+  function shareOnWhatsapp() {
+    // Replace "YOUR_URL" with the actual URL of your blog post
+    var url = `https://pypixel.com/post/${blogPost.slug}`;
+    window.open("https://wa.me/?text=" + encodeURIComponent(url));
+  }
+  function shareOnLinkedin() {
+    // Replace "YOUR_URL" with the actual URL of your blog post
+    var url = `https://pypixel.com/post/${blogPost.slug}`;
+    var title = blogPost.title;
+    window.open("https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title));
+  }
+  function shareOnTelegram() {
+    // Replace "YOUR_URL" with the actual URL of your blog post
+    var url = `https://pypixel.com/post/${blogPost.slug}`;
+    var title = blogPost.title;
+    window.open("https://t.me/share/url?url=" + encodeURIComponent(url) + "&text=" + encodeURIComponent(title));
+  }
+  function shareOnTwitter() {
+    // Replace "YOUR_URL" with the actual URL of your blog post
+    var url = `https://pypixel.com/post/${blogPost.slug}`;
+    var title = blogPost.title;
+    window.open("https://twitter.com/intent/tweet?url=" + encodeURIComponent(url) + "&text=" + encodeURIComponent(title));
+  }
+
   useEffect(() => {
     const handleRouteChange = (url) => {
       ga.pageview(url);
@@ -51,7 +86,10 @@ export default function Post({ blogPost, relatedPosts, popularPosts }) {
         <title>{blogPost.title}</title>
         <meta name="description" content={description} />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content={`https://pypixel.com/post/${blogPost.slug}`} />
+        <meta
+          property="og:url"
+          content={`https://pypixel.com/post/${blogPost.slug}`}
+        />
         <meta property="og:title" content={blogPost.title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={blogPost.cover_image} />
@@ -61,7 +99,10 @@ export default function Post({ blogPost, relatedPosts, popularPosts }) {
         <meta name="twitter:title" content={blogPost.title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={blogPost.cover_image} />
-        <link rel="canonical" href={`https://pypixel.com/post/${blogPost.slug}`} />
+        <link
+          rel="canonical"
+          href={`https://pypixel.com/post/${blogPost.slug}`}
+        />
       </Head>
       <div className={styles["blog-post-detail-upper-body"]}>
         <main className={styles["blog-post-detail-main"]}>
@@ -69,19 +110,25 @@ export default function Post({ blogPost, relatedPosts, popularPosts }) {
             {blogPost.category.name}
           </p>
           <h1 className={styles["blog-post-detail-title"]}>{blogPost.title}</h1>
-          <h4 className={styles["blog-post-detail-published-date"]}>
-            <AiFillClockCircle className={styles["clock-icon"]} /> Published on{" "}
-            {format(
-              new Date(blogPost.created_on.substring(0, 10)),
-              "dd MMMM yyyy"
-            )}
-          </h4>
-          <div>
+          <div className={styles["blog-post-detail-date-n-share-div"]}>
+            <h4 className={styles["blog-post-detail-published-date"]}>
+              <AiFillClockCircle className={styles["clock-icon"]} /> Published
+              on{" "}
+              {format(
+                new Date(blogPost.created_on.substring(0, 10)),
+                "dd MMMM yyyy"
+              )}
+            </h4>
 
-          {/* <AiFillTwitterCircle/>
-          <BsWhatsapp/>
-          <BsTelegram/> */}
+            <div className={styles['social-share-icons']} >
+              <BsFacebook className={styles['share-icon']} onClick={shareOnFacebook}/>
+              <BsTwitter className={styles['share-icon']} onClick={shareOnTwitter}/>
+              <BsWhatsapp className={styles['share-icon']} onClick={shareOnWhatsapp}/>
+              <BsTelegram className={styles['share-icon']} onClick={shareOnTelegram}/>
+              <BsLinkedin className={styles['share-icon']} onClick={shareOnLinkedin}/>
+            </div>
           </div>
+
           <Image
             src={blogPost.cover_image}
             className={styles["blog-post-detail-img"]}
@@ -97,9 +144,8 @@ export default function Post({ blogPost, relatedPosts, popularPosts }) {
           ></div>
         </main>
       </div>
-      <hr className={styles['hr-style']}></hr>
+      <hr className={styles["hr-style"]}></hr>
       <section className={gridStyles["posts-list-container"]}>
-        
         <h2 className={styles["popular-posts-heading"]}>
           Popular Posts in {blogPost.category.name}
         </h2>
@@ -131,7 +177,9 @@ export default function Post({ blogPost, relatedPosts, popularPosts }) {
                     <h2 className={gridStyles["grid-card-post-title"]}>
                       {truncateString(post.title, 70)}
                     </h2>
-                    <p className={gridStyles["grid-post-meta-descr"]}>{post.meta_description}</p>
+                    <p className={gridStyles["grid-post-meta-descr"]}>
+                      {post.meta_description}
+                    </p>
                   </Link>
                 </div>
               );
